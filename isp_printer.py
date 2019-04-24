cc837069fc6f6fde200d1402d68ae7abf012514e
Pratyush Mohapatra <ppmvcs@gmail.com>
2019-04-24 20:55:10 +0530

    removed default verbose output

--- isp_printer.py
+++ isp_printer.py
@@ -2,8 +2,18 @@ from colorama import Fore, Back, Style
 
 
 def print_this(response_dict):
+    pass
+    if response_dict['Status'] != 'Active' or int(response_dict['Pending Amount']) > 0:
+        print('Pending Amount: ' + Fore.RED + Style.BRIGHT + response_dict['Pending Amount'] + Style.RESET_ALL)
+        print('Status: ' + Fore.RED + Style.BRIGHT + response_dict['Status'] + Style.RESET_ALL)
+    else:
+        print('Status: ' + Fore.GREEN + Style.BRIGHT + response_dict['Status'] + Style.RESET_ALL)
+        print('Pending Amount: ' + response_dict['Pending Amount'])
+
+def print_verbose(response_dict):
     for keys,values in response_dict.items():
         print(keys + ": " + values)
     
-    if int(response_dict['Pending Amount']) > 0:
-        print("Pending Amount: " + Fore.RED + Style.BRIGHT + response_dict['Pending Amount'] + Style.RESET_ALL)
+    if response_dict['Status'] != 'Active' or int(response_dict['Pending Amount']) > 0:
+        print('Pending Amount: ' + Fore.RED + Style.BRIGHT + response_dict['Pending Amount'] + Style.RESET_ALL)
+        print('Status: ' + Fore.RED + Style.BRIGHT + response_dict['Status'] + Style.RESET_ALL)
