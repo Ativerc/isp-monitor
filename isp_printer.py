cd201e0ad77e3c23cbf44966efea0b79bf2a12cd
Pratyush Mohapatra <ppmvcs@gmail.com>
2019-05-25 13:42:05 +0530

    For both Active/Inactive states, 'Satus' and 'Pending Amount' in one order.

--- isp_printer.py
+++ isp_printer.py
@@ -4,16 +4,18 @@ from colorama import Fore, Back, Style
 def print_this(response_dict):
     pass
     if response_dict['Status'] != 'Active' or int(response_dict['Pending Amount']) > 0:
-        print('Pending Amount: ' + Fore.RED + Style.BRIGHT + response_dict['Pending Amount'] + Style.RESET_ALL)
         print('Status: ' + Fore.RED + Style.BRIGHT + response_dict['Status'] + Style.RESET_ALL)
+        print('Pending Amount: ' + Fore.RED + Style.BRIGHT + response_dict['Pending Amount'] + Style.RESET_ALL)
+        
     else:
         print('Status: ' + Fore.GREEN + Style.BRIGHT + response_dict['Status'] + Style.RESET_ALL)
         print('Pending Amount: ' + response_dict['Pending Amount'])
 
-def print_verbose(response_dict):
+def print_verbose(response_dict): #TODO make verbose give 'Status'/'Pending Amount' in red and green depending on Status
     for keys,values in response_dict.items():
-        print(keys + ": " + values)
+        print(keys + ": " + values) 
     
     if response_dict['Status'] != 'Active' or int(response_dict['Pending Amount']) > 0:
-        print('Pending Amount: ' + Fore.RED + Style.BRIGHT + response_dict['Pending Amount'] + Style.RESET_ALL)
         print('Status: ' + Fore.RED + Style.BRIGHT + response_dict['Status'] + Style.RESET_ALL)
+        print('Pending Amount: ' + Fore.RED + Style.BRIGHT + response_dict['Pending Amount'] + Style.RESET_ALL)
+        
