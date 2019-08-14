a02279ab652f0301722aee3d1afad76962eb8d49
Pratyush Mohapatra <ppmvcs@gmail.com>
2019-08-14 22:53:50 +0530

    Made some changes. Can't remember what changes were made. Sorry!

--- isp_monitor.py
+++ isp_monitor.py
@@ -1,19 +1,21 @@
+#!/bin/python
+
 import requests, argparse, getpass
 from ignore.credentials import credentials_dict
 from ignore.defaults import headers
 from isp_parser import page_parser
 from isp_printer import print_this, print_verbose
-from datetime_proxy import months as dtproxy_months
+
 
 def passwordFetcher():
   password = getpass.getpass(prompt="Enter your password: ")
-  password_confirm = getpass.getpass(prompt="Confirm password: ")
-  while password != password_confirm:
-    print("Passwords didn't match! Please re-enter your passwords again!")
-    password = getpass.getpass(prompt="Enter your password: ")
-    password_confirm = getpass.getpass(prompt="Confirm password: ")
-  else:
-    print("Password confirmed!")
+  # password_confirm = getpass.getpass(prompt="Confirm password: ")
+  # while password != password_confirm:
+  #   print("Passwords didn't match! Please re-enter your passwords again!")
+  #   password = getpass.getpass(prompt="Enter your password: ")
+  #   password_confirm = getpass.getpass(prompt="Confirm password: ")
+  # else:
+  #   print("Password confirmed!")
   return password
 
 
@@ -24,16 +26,19 @@ def instant_credentials_fetch():
   fetched_creds = {'username': username, 'password': password}
   return fetched_creds 
 
-
+#Argument Parser Code
 parser = argparse.ArgumentParser(description="Get data from Jetspot's Account Page.")
 parser.add_argument('-i', '--instant', action='store_true')
 parser.add_argument('-v', '--verbose', action='store_true')
 args = parser.parse_args()
 
+#Start Session with ISP's Login Page
 s = requests.session()
 login_url = credentials_dict['login_url']
+initial_response = s.get(login_url, verify=False)
 
 
+#ifelse for credentials handling in "instant" vs "non instant" mode of operation
 if args.instant == True:
   instant_creds = instant_credentials_fetch()
   username = instant_creds['username']
@@ -43,34 +48,19 @@ else:
   username = credentials_dict['username'] 
   password = credentials_dict['password']
 
-
-initial_response = s.get(login_url, verify=False)
-
-
 cookiejar = s.cookies
-
 data = {
   'LoginForm[username]': username,
   'LoginForm[password]': password,
   'yt0': 'Log In'
 }
-
-headers = headers
-
-print("Fetching data....")
 response = s.post(login_url, cookies=cookiejar, data=data, headers= headers, verify=False)
 
 response_dict = (page_parser(response))
-# Add datetime module
-# can't find datetime module's docs at the moment. 
-# Let's implement some of my own stuff
-# heck I hope response_dict has a key named "Last Date"
-date = response_dict["Plan Expiry Date"]
-month = date.split("-")[1]
-month_name = dtproxy_months[month]
-print(date.split("-")[0] + " " + month_name + " " + date.split("-")[2])
 
-if args.verbose == True:
-  print_verbose(response_dict)
-else:
-  print_this(response_dict)
+if __name__ == '__main__':
+
+  if args.verbose == True:
+    print_verbose(response_dict)
+  else:
+    print_this(response_dict)
--- isp_printer.py
+++ isp_printer.py
@@ -1,26 +1,30 @@
 from colorama import Fore, Back, Style
-
+from datetime_proxy import months as dtproxy_months
 
 def print_this(response_dict):
-    pass
-    if response_dict['Status'] != 'Active' or int(response_dict['Pending Amount']) > 0: #When Status != Active or Pending Amount > 0
-        print(Fore.WHITE + Style.BRIGHT + '\nStatus: ' + Fore.RED + Style.BRIGHT + response_dict['Status'] + Style.RESET_ALL)
-        print(Fore.WHITE + Style.BRIGHT + 'Pending Amount: ' + Fore.RED + Style.BRIGHT + response_dict['Pending Amount'] + Style.RESET_ALL)       
-    else: 
-        print(Fore.WHITE + Style.BRIGHT + '\nStatus: ' + Fore.GREEN + Style.BRIGHT + response_dict['Status'] + Style.RESET_ALL)
-        print(Fore.WHITE + Style.BRIGHT + 'Pending Amount: ' + Fore.GREEN + Style.BRIGHT + response_dict['Pending Amount'] + Style.RESET_ALL)
-
-def print_verbose(response_dict): #TODO make verbose give 'Status'/'Pending Amount' in red and green depending on Status
-    
     if response_dict['Status'] != 'Active' or int(response_dict['Pending Amount']) > 0: #When Status != Active or Pending Amount > 0
         print(Fore.WHITE + Style.BRIGHT + '\nStatus: ' + Fore.RED + Style.BRIGHT + response_dict['Status'] + Style.RESET_ALL)
         print(Fore.WHITE + Style.BRIGHT + 'Pending Amount: ' + Fore.RED + Style.BRIGHT + response_dict['Pending Amount'] + Style.RESET_ALL)
+        print(Fore.WHITE + Style.BRIGHT + 'Last Date for Payment: ' + Fore.RED + Style.BRIGHT + month_name(response_dict["Plan Expiry Date"]) + Style.RESET_ALL)      
+    else: 
+        print(Fore.WHITE + Style.BRIGHT + '\nStatus: ' + Fore.GREEN + Style.BRIGHT + response_dict['Status'] + Style.RESET_ALL)
+        print(Fore.WHITE + Style.BRIGHT + 'Pending Amount: ' + Fore.GREEN + Style.BRIGHT + response_dict['Pending Amount'] + Style.RESET_ALL)
+        print(Fore.WHITE + Style.BRIGHT + 'Plan Expiry Date: ' + Fore.GREEN + Style.BRIGHT + month_name(response_dict["Plan Expiry Date"]) + Style.RESET_ALL)
+
+def print_verbose(response_dict):
+    
+    if response_dict['Status'] != 'Active' or int(response_dict['Pending Amount']) > 0: #When Status != Active or Pending Amount > 0
+        print_this(response_dict)
         print("\n:-------Details-------:\n")
         for keys,values in response_dict.items():
             print(Fore.WHITE + Style.BRIGHT + keys + Style.RESET_ALL + ": " + values) 
     else:
-        print(Fore.WHITE + Style.BRIGHT + '\nStatus: ' + Fore.GREEN + Style.BRIGHT + response_dict['Status'] + Style.RESET_ALL)
-        print(Fore.WHITE + Style.BRIGHT + 'Pending Amount: ' + Fore.GREEN + Style.BRIGHT + response_dict['Pending Amount'] + Style.RESET_ALL)
+        print_this(response_dict)
         print("\n:-------Details-------:\n")
         for keys,values in response_dict.items():
             print(Fore.WHITE + Style.BRIGHT + keys + Style.RESET_ALL + ": " + values) 
+
+def month_name(last_date):
+    month = last_date.split("-")[1]
+    month_name = dtproxy_months[month]
+    return (last_date.split("-")[0] + " " + month_name + " " + last_date.split("-")[2])
