8fb986d261935aa04d125fd06b8d4c457943811c
Pratyush Mohapatra <ppmvcs@gmail.com>
2019-12-30 17:41:09 +0530

    Changed some variable names. Moved session inside responser function.

--- isp_monitor.py
+++ isp_monitor.py
@@ -1,7 +1,7 @@
 #!/bin/python
 
 import requests, argparse, getpass
-from ignore.credentials import credentials_dict
+from ignore.credentials import stored_creds
 from ignore.defaults import headers
 from isp_parser import page_parser
 from isp_printer import print_this, print_verbose
@@ -19,12 +19,11 @@ def passwordFetcher():
   return password
 
 
-def instant_credentials_fetch():
-  fetched_creds = {}
+def instant_creds_fetcher():
   username = input("Enter your Username/UserID: ")
   password = passwordFetcher()
-  fetched_creds = {'username': username, 'password': password}
-  return fetched_creds 
+  instant_fetched_creds = {'username': username, 'password': password}
+  return instant_fetched_creds 
 
 #Argument Parser Code
 parser = argparse.ArgumentParser(description="Get data from Jetspot's Account Page.")
@@ -32,31 +31,31 @@ parser.add_argument('-i', '--instant', action='store_true')
 parser.add_argument('-v', '--verbose', action='store_true')
 args = parser.parse_args()
 
-#Start Session with ISP's Login Page
-s = requests.session()
-login_url = credentials_dict['login_url']
-initial_response = s.get(login_url, verify=False)
-
-
-#ifelse for credentials handling in "instant" vs "non instant" mode of operation
+#ifelse for credentials handling in "instant" vs "non-instant" mode of operation
 if args.instant == True:
-  instant_creds = instant_credentials_fetch()
+  instant_creds = instant_creds_fetcher()
   username = instant_creds['username']
   password = instant_creds['password']
-  args.verbose = True
+  args.verbose = True  # If Instant option is chosen, then output mode is immediately set to verbose
 else:
-  username = credentials_dict['username'] 
-  password = credentials_dict['password']
+  username = stored_creds['username'] 
+  password = stored_creds['password']
 
-cookiejar = s.cookies
-data = {
-  'LoginForm[username]': username,
-  'LoginForm[password]': password,
-  'yt0': 'Log In'
-}
+
+
 
 def responser():
-  response = s.post(login_url, cookies=cookiejar, data=data, headers= headers, verify=False)
+  #Start Session with ISP's Login Page
+  s = requests.session()
+  login_url = stored_creds['login_url']
+  #initial_response = s.get(login_url, verify=False)
+  cookiejar = s.cookies
+  data = {
+    'LoginForm[username]': username,
+    'LoginForm[password]': password,
+    'yt0': 'Log In'
+  }
+  response = s.post(login_url, cookies=cookiejar, data=data, headers=headers, verify=False)  #ssl_certificate verification fails for some reason; hence set verify=False
   response_dict = (page_parser(response))
   return response_dict
 
