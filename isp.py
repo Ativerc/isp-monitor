68cace3e54909a94833d722502505ce41bfea6f1
Pratyush Mohapatra <ppmvcs@gmail.com>
2020-05-24 17:20:13 +0530

    Groundwork for multiple ISP support. Some problems. But working!

--- isp.py
+++ isp.py
@@ -1,68 +1,40 @@
 #!/bin/python
 
 import requests, argparse, getpass, os
-from ignore.defaults import headers
-from isp_parser import page_parser
 from isp_printer import print_this, print_verbose
+from isps.jetspot.jetspot import jetspot_main
 
+isp_options = {1: "Jetspot"}
 
-def passwordFetcher():
-  password = getpass.getpass(prompt="Enter your password: ")
-  password_confirm = getpass.getpass(prompt="Confirm password: ")
-  while password != password_confirm:
-    print("Passwords didn't match! Please re-enter your passwords again!")
-    password = getpass.getpass(prompt="Enter your password: ")
-    password_confirm = getpass.getpass(prompt="Confirm password: ")
-  else:
-    print("Password confirmed!")
-  return password
-
-
-def instant_creds_fetcher():
-  username = input("Enter your Username/UserID: ")
-  password = passwordFetcher()
-  instant_fetched_creds = {'username': username, 'password': password}
-  return instant_fetched_creds 
+def isp_selector(): # Asks user for their ISP
+  for i, j in isp_options.items():
+    print(f"{i}. {j}")
+  confirm=""
+  selected=""
+  while confirm != "Y": 
+    selected = input("Enter the corresponding number for the ISP.\n")
+    confirm = input(f"Selected Option: {selected} which is for: " +
+    f"{isp_options[int(selected)]} \nConfirm your ISP is {isp_options[int(selected)]} ? Y/N: ") 
+  return selected
 
 #Argument Parser Code
-parser = argparse.ArgumentParser(description="Get data from Jetspot's Account Page.")
-parser.add_argument('-i', '--instant', action='store_true')
+parser = argparse.ArgumentParser(description="Get data from ISP's Account Page.")
+parser.add_argument('-i', '--interactive', action='store_true')
 parser.add_argument('-v', '--verbose', action='store_true')
 args = parser.parse_args()
 
-#ifelse for credentials handling in "instant" vs "normal" mode of operation
+#ifelse for credentials handling in "interactive" vs "normal" mode of operation
 
-if args.instant == True: # If "instant" option is chosen; instant_creds_fetcher() is called; output mode is set to verbose
-  instant_creds = instant_creds_fetcher()
-  username = instant_creds['username']
-  password = instant_creds['password']
+if args.interactive == True: # If "interactive" option is chosen; instant_creds_fetcher() is called; output mode is set to verbose
+  selected_isp = isp_selector()
   args.verbose = True  
 else: # for "normal" mode, the credentials are fetched from the environment variables
-  username = os.environ['USERNAME'] 
-  password = os.environ['PASSWORD']
-
-
-
-
-def responser():
-  #Start Session with ISP's Login Page
-  s = requests.session()
-  login_url = os.environ['URL']
-  #initial_response = s.get(login_url, verify=False)
-  cookiejar = s.cookies
-  data = {
-    'LoginForm[username]': username,
-    'LoginForm[password]': password,
-    'yt0': 'Log In'
-  }
-  response = s.post(login_url, cookies=cookiejar, data=data, headers=headers, verify=False)  #ssl_certificate verification fails for some reason; hence set verify=False
-  response_dict = (page_parser(response))
-  return response_dict
-
+  selected_isp = os.environ['ISP']
 
 if __name__ == '__main__':
-
-  if args.verbose == True:
-    print_verbose(responser())
-  else:
-    print_this(responser())
+  if (selected_isp == 1 or "JETSPOT"):
+      jetspot_main(args.verbose, args.interactive)
+  # if args.verbose == True:
+  #   print_verbose(responser())
+  # else:
+  #   print_this(responser())
---
+++ isps/jetspot/jetspot.py
@@ -0,0 +1,78 @@
+import requests, os
+from bs4 import BeautifulSoup as bs
+import getpass
+
+
+def passwordFetcher():
+    password = getpass.getpass(prompt="Enter your password: ")
+    password_confirm = getpass.getpass(prompt="Confirm password: ")
+    while password != password_confirm:
+        print("Passwords didn't match! Please re-enter your passwords again!")
+        password = getpass.getpass(prompt="Enter your password: ")
+        password_confirm = getpass.getpass(prompt="Confirm password: ")
+    else:
+        print("Password confirmed!")
+        return password
+# too many confirmations. Need to rewrite the whole input mechanism.
+
+
+def instant_creds_fetcher(): # Should change its name probably? :TODO:
+    username = input("Enter your Username/UserID: ")
+    password = passwordFetcher()
+    instant_fetched_creds = {'username': username, 'password': password}
+    return instant_fetched_creds 
+
+def page_parser(response): # parses the html response; passes data to formatter function for formatting
+    soup = bs(response.content, features="html.parser")
+    table_datas = soup.select('td')
+    return formatter(table_datas)
+
+
+def tag_to_string(tag_item): # converts bs4.element.tag datatype to string datatype
+    string = tag_item.get_text()
+    return string
+
+
+def formatter(table_data):
+    data_list = list(map(tag_to_string, table_data)) # maps each list item to tag_to_string function;  # converts the returned data to list
+    counter = 0
+    while "" in data_list and counter<6: #removes empty "" from data_list 
+        data_list.remove("")
+        counter +=1
+    non_empty_data_list = list(map(str.strip, data_list))
+    non_empty_data_list.append("") 
+    data_dict = {}
+    for i in range(0, len(non_empty_data_list), 2):
+        data_dict[non_empty_data_list[i]] = non_empty_data_list[i+1]
+    return data_dict
+
+
+def responser(username, password):
+    #Start Session with ISP's Login Page
+    session = requests.session()
+    login_url = 'https://login.jetspot.in/synnefoclient/'
+    #initial_response = s.get(login_url, verify=False)
+    cookiejar = session.cookies
+    data = {
+        'LoginForm[username]': username,
+        'LoginForm[password]': password,
+        'yt0': 'Log In'
+        }
+    print("Fetching Data!")
+    response = session.post(login_url, cookies=cookiejar, data=data, verify=False)  #ssl_certificate verification fails for some reason; hence set verify=False
+    response_dict = (page_parser(response))
+    return response_dict
+
+
+def jetspot_main(verbosity=False, interactive=False):
+    if interactive == False:
+        username = os.environ['USERNAME'] 
+        password = os.environ['PASSWORD']
+    else:
+        fetched_creds = instant_creds_fetcher()
+        username = fetched_creds['username']
+        password = fetched_creds['password']
+    data = responser(username, password)
+    for key, value in data.items(): # delete post testing :TODO:
+        print(f"{key}: {value}")    # delete post testing :TODO:
+    return data
