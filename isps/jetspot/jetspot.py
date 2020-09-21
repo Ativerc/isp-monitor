6e6592ec971d9b2c30d921250ae5f4985c23dfbd
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2020-09-21 15:55:00 +0530

    Reformatting(Not the best choice of words) method improved

--- isps/jetspot/jetspot.py
+++ isps/jetspot/jetspot.py
@@ -1,4 +1,4 @@
-import requests, os, getpass
+import requests, os, getpass, sys
 from bs4 import BeautifulSoup as bs
 
 
@@ -41,33 +41,68 @@ def tag_to_string(tag_item):
     string = tag_item.get_text()
     return string
 
+usual_key_values = ["Status", "Name", "Plan Name", 
+    "Plan Renewal Date", "Plan Expired On", "Pending Amount",
+    "Current Complaint Status", "Service Provider Details"]
 
-def formatter(table_data):
+def key_checker(td_string_list):
     """
-    maps each list item to tag_to_string function
-    converts the returned data to list
+    Checks if the values from the td_string_list to be used as keys 
+    in the table_data_dict have been changed and if changed, 
+    raise an Error!
     """
-    data_list = list(map(tag_to_string, table_data)) 
-    print(data_list) #test
-    for i in data_list:
-        print(i)
-    counter = 0
-    while "" in data_list and counter<6: #removes empty "" from data_list 
-        data_list.remove("")
-        counter +=1
-    print("Data List 2") #test
-    print(data_list)
-    non_empty_data_list = list(map(str.strip, data_list))
-    print("nedl 1") #test
-    for i in non_empty_data_list: 
-        print(i)
-    # non_empty_data_list.append("") 
-    print("nedl 2") #test
-    for i in non_empty_data_list:
-        print(i)
+    # key_position_match_dict usual_key_value position : td_string_list position
+    # key_position_match_dict = {0: 0, 1:2, 2:4, 3:6, 4:8, 5:10, 6:12, 7:14}
+    failed_keys = []
+    for i in range(len(usual_key_values)):
+        if usual_key_values[i] not in td_string_list:
+            failed_keys.append(i)
+            # Raise here or later?
+    if len(failed_keys) != 0:
+        print("KeyNotFoundError by key_checker(). Keys not found are:", end="") # :TODO: Raise Error!
+        for i in range(len(failed_keys)):
+            print(f"\"{usual_key_values[i]}\" ", end="")
+        print("Exiting!")
+        sys.exit()
+    return td_string_list
+
+    
+def value_data_extractor(td_data_list):
+    string_data_list = []
+    for i in range(len(usual_key_values)):
+        if usual_key_values[i] in td_data_list:
+            string_data_list.append(td_data_list[td_data_list.index(usual_key_values[i]) + 1])
+        else:
+            print("Key not found! by list_data_extractor! Exiting!") # :TODO: Raise Error!
+            sys.exit()
+    stripped_string_data_list = list(map(str.strip, string_data_list))
+    return stripped_string_data_list
+
+
+def data_dict_maker(key_list, value_list):
     data_dict = {}
-    for i in range(0, len(non_empty_data_list)-2, 2):
-        data_dict[non_empty_data_list[i]] = non_empty_data_list[i+1]
+    key_list_len = len(key_list)
+    value_list_len = len(value_list)
+    if key_list_len != value_list_len:
+        print("KeyValueListLengthMismatchError: by data_dict_maker") # :TODO: Raise Error!
+        print(f"Key List Length {key_list_len} != Value List Length {value_list_len}") 
+        print("Exiting!")
+        sys.exit()
+    else:
+        for i in range(key_list_len):
+            data_dict[key_list[i]] = value_list[i]
+    return data_dict
+
+
+def formatter(td_tags_list):
+    """
+    Main function for formatting. 
+    Individual jobs are done by other functions: key_checker(), value_data_extractor(), data_dict_maker()
+    """
+    td_string_list = list(map(tag_to_string, td_tags_list))
+    key_present_list = key_checker(td_string_list) # Check if key is not present
+    value_data_list = value_data_extractor(key_present_list)
+    data_dict = data_dict_maker(usual_key_values, value_data_list)
     return data_dict
 
 # REQUEST/RESPONSE PART
