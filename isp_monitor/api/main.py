d29d13c90c69325df10f03c61914424049cc1877
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2023-03-19 02:53:55 +0530

    bug: ModuleNotFoundError bugs in /api/main.py

---
+++ isp_monitor/api/main.py
@@ -0,0 +1,32 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+from isp_monitor.isps.railwire.railwire_raw import raw_input
+
+class Item(BaseModel):
+    service: str
+    state: Union[str, None] = None
+    url: str
+    userid: str
+    password: str
+    output_type: Union[str, None] = None
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.post("/raw/")
+async def raw_info(item: Item):
+    raw_input(item.userid, item.password, item.url, item.output_type)
--- isp_monitor/isps/railwire/railwire_raw.py
+++ isp_monitor/isps/railwire/railwire_raw.py
@@ -9,7 +9,7 @@ from railwire_datacontrol import return_dict
 def raw_input(username, password, url, dict_type):
     response_soup = perform_login(username, password, url)
     data_dict = start_pipeline(response_soup)
-    print(return_dict(dict_type, data_dict))
+    return return_dict(dict_type, data_dict)
 
 if __name__ == "__main__":
     raw_input(os.environ['rw_username'], os.environ['rw_password'], os.environ['rw_url'], "core")
